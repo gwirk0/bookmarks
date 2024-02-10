f6b7aab2e776e2fb5dbb93ec7f7ae275f9e5884a
gwirk0 <152970502+gwirk0@users.noreply.github.com>
2024-02-10 12:42:08 -0500

    Initial commit

---
+++ account/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('You are now logged in!')
+                else:
+                    return HttpResponse('Your account is disabled.')
+            else:
+                return HttpResponse('Invalid username and/or password.')
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
+
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                    'account/dashboard.html',
+                    {'section': 'dashboard'})
+def logout_view(request):
+    logout(request)
+    return redirect('/') # на главную страницу сайта
